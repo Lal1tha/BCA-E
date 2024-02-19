d571fffc70ff8079baa8f2ceec926f5f9ba0bc83
Lal1tha <lalitha19n@gmail.com>
2024-02-19 23:45:15 +0530

    changed

--- add.c
+++ add.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, 
+    int num1, num2,result,sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    scanf("%d", &num2)
     
     sum = num1 + num2;
     
